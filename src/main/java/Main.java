3ac7069c0021a8430f6c3e6f19f175a15e1012a1
aguerin1 <aguerin@unomaha.edu>
2023-11-16 15:34:03 -0600

    Bibash worked on updating the stalled reports to account for date created and added a scrolling feature based on typing on the screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -12,6 +13,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Stage window;
+    private static String keyword;
+    private Controller controller;
 
     public static void main(String[] args) {
         launch(args);
@@ -26,9 +29,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
         Parent root = fxmlLoader.load();
+        controller = fxmlLoader.getController();
         window = primaryStage;
 
         Scene scene = new Scene(root, 1200, 700);
+
+        //Bibash sets an event handler that responds to key typing events within the scene
+        keyword = "";
+        scene.setOnKeyTyped(this::handleKeyTyped);
 
         window.setTitle("Dragon's Lair Pull List");
 
@@ -36,5 +44,21 @@ public class Main extends Application {
         window.setMinHeight(700);
         window.setMinWidth(1200);
         window.show();
+    }
+
+    //Bibash
+    private void handleKeyTyped(KeyEvent event) {
+        char typedChar = event.getCharacter().charAt(0);
+        keyword += typedChar;
+        System.out.println(keyword);
+        if (controller.currentPage == Controller.CURRENT_PAGE.CUSTOMER && controller.getSelectedCustomer() != null)
+            controller.handleCustomerJumping(keyword);
+        if (controller.currentPage == Controller.CURRENT_PAGE.TITLE && controller.getSelectedTitle() != null)
+            controller.handleTitleJumping(keyword);
+    }
+
+
+    public static void clearKeyword() {
+        keyword = "";
     }
 }
